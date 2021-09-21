1098cc9780c69ee579fa9128f9b1d3f37d8b3239
Jenkins <alm.jenkins@viavarejo.com.br>
2021-09-21 10:52:05 -0300

    Analisando resultado

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,17 @@ dotnet tool install -g dotnet-stryker
 dotnet tool restore
 
 cd *.Test/
-dotnet stryker --reporters "['json']"
+dotnet stryker --reporters "['json']" > mutation-result.txt
+
+VAR1="The final mutation score is 100.00 %"
+
+while read p; do
+  echo "$p"
+  if [[ $p == *"$VAR1"* ]]; then
+    echo "SUCESSO"
+    exit 0
+  else
+    echo "FALHA"
+    exit 1
+  fi
+done < mutation-result.txt
